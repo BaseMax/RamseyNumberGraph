9f345fd9e595a4917706f6cebd40101c43a3a17a
Max Base <MaxBaseCode@Gmail.Com>
2019-07-15 16:03:25 +0430

    Update ramsey_5_5.py

--- ramsey_5_5.py
+++ ramsey_5_5.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 ###
 #
-# @Name : RamseyNumber/ramsey_5_5.py
+# @Name : RamseyNumberPython/ramsey_5_5.py
 # @Version : 1.0
 # @Programmer : Max
 # @Date : 2019-07-15
-# @Released under : https://github.com/BaseMax/MongoDbPython/blob/master/LICENSE
-# @Repository : https://github.com/BaseMax/RamseyNumber
+# @Released under : https://github.com/BaseMax/RamseyNumberPython/blob/master/LICENSE
+# @Repository : https://github.com/RamseyNumberPython/RamseyNumber
 #
 ###
 import math
